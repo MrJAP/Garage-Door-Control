3abf31262c55fe59bbe867bb4e140955c8e4f892
MrJAP <mrjap123456@gmail.com>
2022-03-30 20:53:12 -0400

    Initial Project Check-in

---
+++ src/GarageDoorControl/GarageDoorControl.Common/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageDoorControl.Common
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string FavoriteColor { get; set; }
+        public string FavoriteIceCreamFlavor { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public TimeSpan Age => DateTime.UtcNow - DateOfBirth;
+
+    }
+}
---
+++ src/GarageDoorControl/GarageDoorControl.Test/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GarageDoorControl.Common;
+
+var people = new List<Person>();
+people.Add(new Person { Name = "Kelly Smith", FavoriteColor = "Orange", FavoriteIceCreamFlavor = "Chocolate", DateOfBirth = DateTime.Parse("12/15/1968") });
+people.Add(new Person { Name = "Sam Richards", FavoriteColor = "Red", FavoriteIceCreamFlavor = "Mint", DateOfBirth = DateTime.Parse("4/1/1996") });
+
+
+foreach (var person in people)
+{
+    Console.WriteLine(person.Name + "'s favorite color is " + person.FavoriteColor + " and they like " + person.FavoriteIceCreamFlavor + " ice cream, and is " + person.Age.Days + " days old.");
+}
+
+
